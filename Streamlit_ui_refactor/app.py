656c1c99e4dacedbb889ddd1e221e2ef1a0557d3
Sohamcodesstuff <107258799+Sohamcodesstuff@users.noreply.github.com>
2023-12-24 10:26:46 +0530

    progress bar removed "text" argument

--- Streamlit_ui_refactor/app.py
+++ Streamlit_ui_refactor/app.py
@@ -41,7 +41,7 @@ def main():
 
             for percent_complete in range(100):
                 time.sleep(0.01)
-                my_bar.progress(percent_complete + 1, text=progress_text)
+                my_bar.progress(percent_complete + 1)
             time.sleep(1)
             my_bar.empty()
             if(prediction[0]==0):
