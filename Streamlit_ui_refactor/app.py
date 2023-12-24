a4b0729a2644948c8b1fb6034c9eac23bb5ebe44
Sohamcodesstuff <107258799+Sohamcodesstuff@users.noreply.github.com>
2023-12-24 10:29:59 +0530

    Update app.py

--- Streamlit_ui_refactor/app.py
+++ Streamlit_ui_refactor/app.py
@@ -41,7 +41,7 @@ def main():
 
             for percent_complete in range(100):
                 time.sleep(0.01)
-                my_bar.progress(percent_complete + 1)
+                my_bar.progress(percent_complete + 1,text=progress_text)
             time.sleep(1)
             my_bar.empty()
             if(prediction[0]==0):
