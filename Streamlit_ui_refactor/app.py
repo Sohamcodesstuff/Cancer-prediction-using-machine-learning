96bdfcd45bac1a67b05acf2bdacd2b7a89665d17
Sohamcodesstuff <107258799+Sohamcodesstuff@users.noreply.github.com>
2023-12-24 10:23:34 +0530

    Fixed numerical input value type issue

--- Streamlit_ui_refactor/app.py
+++ Streamlit_ui_refactor/app.py
@@ -18,16 +18,16 @@ def main():
     
     if choice=="Predict":
         st.subheader("Input Biopsy Parameters")
-        p1 = st.number_input("Texture Mean", min_value=9.71, max_value=39.2, value=min)
-        p2 = st.number_input("Area Mean", min_value=143.5, max_value=2501.00, value="min")
-        p3 = st.number_input("Concavity Mean", min_value=0.00, max_value=0.43, value="min")
-        p4 = st.number_input("Area SE", min_value=6.80, max_value=542.20, value="min")
-        p5 = st.number_input("Concavity SE", min_value=0.00, max_value=0.40, value="min")
-        p6 = st.number_input("Fractal Dimension SE", min_value=0.00, max_value=0.03, value="min")
-        p7 = st.number_input("Smoothness Worst", min_value=0.07, max_value=0.22, value="min")
-        p8 = st.number_input("Concavity Worst", min_value=0.00, max_value=1.25, value="min")
-        p9 = st.number_input("Symmetry Worst", min_value=0.16, max_value=0.67, value="min")
-        p10 = st.number_input("Fractal Dimension Worst", min_value=0.06, max_value=0.21, value="min")
+        p1 = st.number_input("Texture Mean", min_value=9.71, max_value=39.2, value=9.71)
+        p2 = st.number_input("Area Mean", min_value=143.5, max_value=2501.00, value=143.5)
+        p3 = st.number_input("Concavity Mean", min_value=0.00, max_value=0.43, value=0.00)
+        p4 = st.number_input("Area SE", min_value=6.80, max_value=542.20, value=6.80)
+        p5 = st.number_input("Concavity SE", min_value=0.00, max_value=0.40, value=0.00)
+        p6 = st.number_input("Fractal Dimension SE", min_value=0.00, max_value=0.03, value=0.00)
+        p7 = st.number_input("Smoothness Worst", min_value=0.07, max_value=0.22, value=0.07)
+        p8 = st.number_input("Concavity Worst", min_value=0.00, max_value=1.25, value=0.00)
+        p9 = st.number_input("Symmetry Worst", min_value=0.16, max_value=0.67, value=0.16)
+        p10 = st.number_input("Fractal Dimension Worst", min_value=0.06, max_value=0.21, value=0.06)
 
         params = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]
 
